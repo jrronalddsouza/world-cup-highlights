dc40f0ce7c65cb341871ece5d8ffc017eb87a55c
groupincworld <98367591+groupincworld@users.noreply.github.com>
2022-11-25 02:08:04 +0530

    update

--- node_modules/@layer0/cli/commands/env.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-exports.command = 'env';
-exports.describe = "Allows to manage site's environments and perform actions with them";
-
-exports.builder = yargs => yargs.commandDir('env').demandCommand();
